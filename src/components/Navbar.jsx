32f8e8519006a0dba9b25566fb5b7cfa79118a51
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-19 02:46:41 +0530

    Readme Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FaTimes, FaBars } from 'react-icons/fa';
+import { FaBars } from 'react-icons/fa';
+// FaTimes,
 import Logo from '../assets/logo.png';
 const Navbar = () => {
   return (
