03b5920e92b94b30ea7af8ae0b048e01221f28e6
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-19 02:44:04 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaTimes, FaBars } from 'react-icons/fa';
+import Logo from '../assets/logo.png';
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="logo">
+          <Link to="/">
+            <img src={Logo} alt="company logo" />
+          </Link>
+        </div>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link tp="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link tp="/" className="nav-link">
+              Pricing
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link tp="/" className="nav-link">
+              FAQ
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link tp="/" className="nav-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div className="hamburger">
+          <FaBars />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
